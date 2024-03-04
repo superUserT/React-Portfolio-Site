c0eb28831ead32b9b28fa38092cf3a31e6f9a5f5
superUserT <rantshothabisomail@gmail.com>
2024-03-04 18:13:27 +0200

    newresume

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is my portfolio site created mostly in JS. This repo contain the boilerplate of this site.
+
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CVThabisoRantsho.pdf";
+
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-
 pdfjs.GlobalWorkerOptions.workerSrc = `https://drive.google.com/uc?export=download&id=1HDis8qHCmaXCcQgdG9ju3TWWUoh67O7Z`;
+
 
 const resumeLink = "https://drive.google.com/file/d/1HDis8qHCmaXCcQgdG9ju3TWWUoh67O7Z";
 
